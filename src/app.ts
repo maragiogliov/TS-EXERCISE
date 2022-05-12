6fa0a996d62721920915569b6ba035989857a380
nonexistentdodo <nonexistentdodo@protonmail.com>
2022-05-12 12:21:03 +0200

    Acces Modifiers

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,11 @@
 // console.log(anchor.href)
 
 // const form = document.querySelector('form')!;
-const form = document.querySelector('.new-item-form')!;
+const formTest = document.querySelector('.new-item-form')!;
 // console.log(form)
 
 // We can use "as HTMLFormElement" type, so we can use all the properties and methods inside VsCode
-const formTwo = document.querySelector('.new-item-form') as HTMLFormElement;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(formTwo.children)
 
 // Now let's grab each input fields
@@ -28,3 +28,90 @@ form.addEventListener('submit',(e)=>{
         amount.valueAsNumber
     );
 })
+
+// Classes: A blueprint for an object
+
+// class invoice {
+//     public client:string;
+//     public details:string;
+//     public amount:number;
+
+//     constructor ( c: string, d: string, a:number){
+//         this.client = c;
+//         this.details = d;
+//         this.amount = a;
+//     };
+
+//     format(){
+//         return `${this.client} owes £${this.amount} for ${this.details}`;
+//     };
+// };
+
+// and that is a class with a format method added, now if we want to instantiate that class and create an object based on it we:
+
+// const invOne = new invoice('mario','work on the mario website', 450);
+// const invTwo = new invoice('luigi','work on the luigi website', 700);
+
+// // we can also defined what type of data or class we want the object to be for example:
+
+// // let invoices:string[] = [] // only arrays can be pushed
+
+// let invoices:invoice[] = [] // only objects that are created with the class type invoice can be added to the array
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// invoices.forEach(inv =>{
+//     console.log(inv.client, inv.details, inv.amount, inv.format());
+// });
+
+
+// console.log(invoices)
+
+// Now by default when we create objects with this class, all the properties are public, meaning whenever we create an instance of that class we can access and modify it. For example:
+
+// invOne.client = 'yoshi';
+// invTwo.amount = 5;
+
+// console.log(invOne,invTwo);
+
+// to prevent this we can applying 'access modifiers' in the class (line 35 to 37) we have three types: public, private and readonly. 
+
+// if we use acces modifiers we can change the structure of our class / constructure:
+
+// Classes: A blueprint for an object
+
+class invoice {
+    // public client:string;
+    // public details:string;
+    // public amount:number;
+
+    constructor (
+        readonly client : string,
+        public details : string,
+        public amount : number,
+    ){};
+
+    format(){
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    };
+};
+
+// and that is a class with a format method added, now if we want to instantiate that class and create an object based on it we:
+
+const invOne = new invoice('mario','work on the mario website', 450);
+const invTwo = new invoice('luigi','work on the luigi website', 700);
+
+// we can also defined what type of data or class we want the object to be for example:
+
+// let invoices:string[] = [] // only arrays can be pushed
+
+let invoices:invoice[] = [] // only objects that are created with the class type invoice can be added to the array
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach(inv =>{
+    console.log(inv.client, inv.details, inv.amount, inv.format());
+});
+
+
+console.log(invoices)
